685090e0366e42eb335fc625ce4aa1d609829888
MOHAMED OMAR S/O ASHRAFF ALI <20048218@myrp.edu.sg>
2021-04-30 09:57:29 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20048218/myprojectl02/MainActivity.java
@@ -0,0 +1,16 @@
+package sg.edu.rp.c346.id20048218.myprojectl02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Butts
+    }
+}
